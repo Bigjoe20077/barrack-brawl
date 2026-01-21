1ac15c8f7d700ee4d14e5b0e9290057c3646b6f8
Bigjoe20077 <christianvoll1991@gmail.com>
2026-01-21 13:01:07 +0000

    Fixed Browser Response

--- index.ts
+++ index.ts
@@ -2,20 +2,16 @@ import { Server, Room, Client } from "colyseus";
 import { Schema, type } from "@colyseus/schema";
 import http from "http";
 
-// 1. Der Spiel-Zustand (Was passiert gerade?)
-// Hier speichern wir später Positionen von Einheiten
 class State extends Schema {
     @type("string") myField = "Hallo Welt";
 }
 
-// 2. Der Spiel-Raum (Die Logik)
 class GameRoom extends Room<State> {
     onCreate (options: any) {
         this.setState(new State());
         console.log("Ein neuer Kampf-Raum wurde erstellt!");
 
         this.onMessage("type", (client, message) => {
-            // Hier kommt später die Logik (Einheiten bauen)
             console.log("Nachricht erhalten:", message);
         });
     }
@@ -25,17 +21,17 @@ class GameRoom extends Room<State> {
     }
 }
 
-// 3. Der Server (Das technische Grundgerüst)
 const port = 3000;
+// Der Fix: Wir antworten dem Browser!
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end("Barrack Brawl Server ist ONLINE!");
 });
+
 const gameServer = new Server({
     server: server,
 });
 
-// Wir registrieren den Raum unter dem Namen "battle"
 gameServer.define("battle", GameRoom);
 
 gameServer.listen(port);
